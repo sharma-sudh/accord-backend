c37856a3312ab57ea4752693cebd80dbe5343c25
hszinz <sudhanshulke@gmail.com>
2026-08-26 01:01:03 +0530

    feat(db): set up neon for db

--- src/main/java/com/sudh/accord/AccordBackendApplication.java
+++ src/main/java/com/sudh/accord/AccordBackendApplication.java
@@ -1,7 +1,11 @@
 package com.sudh.accord;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +15,5 @@ public class AccordBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AccordBackendApplication.class, args);
-    }
-
-    @GetMapping
-    public String helloWorld(){
-        return "Hello World!";
     }
 }
